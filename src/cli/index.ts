fce9c0f56bbd2b35c9391ea7630447c2ead9d76d
robojones <contact@jonathan-scholz.de>
2017-12-09 19:32:59 +0100

    add hint; write constants in capslock

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,13 +6,17 @@ import * as path from 'path'
 
 const indent = ' '.repeat(19)
 
-const prefixDescription =
+const MSG =
+  `The dependencies of the given file(s) have been analyzed.
+You need to include the following <script> tags in the <head> section of you website:\n`
+
+const PREFIX_DESCRIPTION =
   `A string that will prepended the urls in the scripttag.
 ${indent}By default the paths look like "/script.js" or "/module/jquery.js"
 ${indent}The prefix can look like "https://example.com/static"
 ${indent}The final URL would then be "https://example.com/static/script.js"`
 
-const suffixDescription =
+const SUFFIX_DESCRIPTION =
   `A string that will appended the urls in the scripttag.
 ${indent}The prefix can look like "?something=1"
 ${indent}The final URL would then be "/script.js?something=1"`
@@ -20,14 +24,16 @@ ${indent}The final URL would then be "/script.js?something=1"`
 interface Options {
   prefix?: string
   suffix?: string
+  hint: boolean
 }
 
 const g = (global as any)
 
 program
   .version(require('../../package.json').version)
-  .option('--prefix <prefix>', prefixDescription)
-  .option('--suffix <suffix>', suffixDescription)
+  .option('--prefix <prefix>', PREFIX_DESCRIPTION)
+  .option('--suffix <suffix>', SUFFIX_DESCRIPTION)
+  .option('--no-hint', 'Hide the message before the script tags.')
   .arguments('[files...]')
   .action((files: string[], options: Options) => {
     const prefix = options.prefix || ''
@@ -65,6 +71,10 @@ program
 
     const snippetPath = require.resolve('../browser/head.min.js')
     const snippet = fs.readFileSync(snippetPath)
+
+    if (options.hint) {
+      console.log(MSG)
+    }
 
     console.log(`<script>\n${snippet}\n</script>`)
 
