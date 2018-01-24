8f0933c9be45a681530b7909a8584be9ed744488
robojones <contact@jonathan-scholz.de>
2018-01-24 10:17:53 +0100

    move storage and list to lib; update list

---
+++ src/lib/List.ts
@@ -0,0 +1,65 @@
+export default class List<T> extends Array<T> {
+  /**
+   * Appends elements to the list if the list does not already contain them.
+   * Returns true if all elements were inserted.
+   * Elements are not inserted if they already exist.
+   * @param elements - Elements that will be appended to the list.
+   * @example
+   * const a = new List(1, 2)
+   * a.add(2) // false
+   * a.add(2, 3) // false
+   * a.add(4) // true
+   */
+  public add (...elements: T[]): boolean {
+    let inserted = true
+
+    for (const element of elements) {
+      if (this.includes(element)) {
+        inserted = false
+        continue
+      }
+
+      this.push(element)
+    }
+
+    return inserted
+  }
+
+  /**
+   * Removes elements from the list.
+   * Returns false if an element was not contained in the list.
+   * @param elements - Elements that will be removed from the list.
+   * @example
+   * const a = new List(1, 2)
+   * a.remove(1) // true
+   * a.remove(2, 3) // false
+   * a.remove(4) // false
+   */
+  public remove (...elements: T[]): boolean {
+    let removed = true
+    for (const element of elements) {
+      const i = this.indexOf(element)
+      if (i === -1) {
+        removed = false
+        continue
+      }
+
+      this.splice(i, 1)
+    }
+
+    return removed
+  }
+
+  /**
+   * Returns a list containing all elements of this list that are not contained in the other array/list.
+   * @param other - The list to compare this list to..
+   * @example
+   * const a = new List(1, 2)
+   * const b = new list(2, 3)
+   * a.uncovered(b) // [ 1 ]
+   */
+  public uncovered (other: T[]): List<T> {
+    const thisToOther = this.filter(element => !other.includes(element))
+    return new List(...thisToOther)
+  }
+}
